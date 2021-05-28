0e751f1dc698caf8bf87a05f538b8a3d4770221a
Felix.Feng <Felix.Feng@sino-bestway.com.cn>
2021-05-28 14:27:37 +0800

    忽略dist目录

--- dist/js/common.js
+++
@@ -1,1 +0,0 @@
-"use strict";var BASE_URL="/api";
--- dist/js/login.js
+++
@@ -1,1 +0,0 @@
-"use strict";var xhr=new XMLHttpRequest;xhr.open("GET","/api/napi/phone/check/rules/"),xhr.send(),xhr.onload=function(){console.log(JSON.parse(xhr.responseText))};
